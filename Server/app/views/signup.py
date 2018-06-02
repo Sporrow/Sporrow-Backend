a095ed8a3e826cf1fe494e6ad5127297633cef6e
JoMingyu <city7310@naver.com>
2018-06-02 21:21:31 +0900

    [DONE] POST /signup

--- Server/app/views/signup.py
+++ Server/app/views/signup.py
@@ -23,3 +23,46 @@ class IDDuplicationCheck(BaseResource):
         else:
             return Response('', 200)
 
+
+@api.resource('/signup')
+class Signup(BaseResource):
+    @json_required({'email': str, 'pw': str})
+    def post(self):
+        def generate_email_certification_code():
+            while True:
+                code = ''.join(random.choice(ascii_uppercase + digits) for _ in range(12))
+
+                if not redis_client.exists(code):
+                    redis_client.set(code, email, ex=60 * 5)
+
+                    return code
+
+        payload = request.json
+
+        email = payload['email']
+        pw = payload['pw']
+
+        if AccountModel.objects(email=email):
+            return Response('', 204)
+
+        redis_client: Redis = current_app.config['REDIS_CLIENT']
+        mail_client: Mail = current_app.config['MAIL_CLIENT']
+
+        code = generate_email_certification_code()
+
+        msg = Message('Please verify user email', sender=current_app.config['MAIL_USERNAME'], recipients=[email])
+        msg.html = '<a href="http://{0}:{1}/certify/{2}">인증하기</a>'.format(
+            current_app.config['REPRESENTATIVE_HOST'] or current_app.config['HOST'],
+            current_app.config['PORT'] if not current_app.testing else 80,
+            code
+        )
+
+        mail_client.send(msg)
+
+        AccountModel(
+            email=email,
+            pw=generate_password_hash(pw)
+        ).save()
+
+        return Response('', 201)
+
