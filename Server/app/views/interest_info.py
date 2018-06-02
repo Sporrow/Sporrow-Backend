339fd62f273d93173296e071bb70c96ff550804d
JoMingyu <city7310@naver.com>
2018-06-03 00:47:30 +0900

    [DONE] GET /interest-list

---
+++ Server/app/views/interest_info.py
@@ -0,0 +1,45 @@
+import random
+import re
+
+from flask import Blueprint, abort, request
+from flask_restful import Api
+
+from app.models.interest import MajorInterestModel, MinorInterestModel
+from app.views import BaseResource
+
+api = Api(Blueprint(__name__, __name__))
+
+
+@api.resource('/interest-list')
+class InterestList(BaseResource):
+    def get(self):
+        """
+        관심사(스포츠 관련 카테고리) 리스트 반환
+        """
+        size = request.args['size']
+
+        if not re.match('\d+', size):
+            abort(400)
+
+        size = int(size)
+
+        major_interest_count = size // 3
+        minor_interest_count = size - major_interest_count
+
+        interests = []
+
+        interests += [
+            {
+                'id': str(interest.id),
+                'name': interest.name
+            } for interest in random.shuffle(MajorInterestModel.objects[:major_interest_count])
+        ]
+
+        interests += [
+            {
+                'id': str(interest.id),
+                'name': interest.name
+            } for interest in random.shuffle(MinorInterestModel.objects[:minor_interest_count])
+        ]
+
+        return self.unicode_safe_json_dumps(interests)
