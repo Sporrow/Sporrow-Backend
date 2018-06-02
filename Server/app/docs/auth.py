8b2f50bf762db539c75124a8ab83238af74aef0e
KingSeojee <30362922+seojeenyeok@users.noreply.github.com>
2018-06-03 00:10:27 +0900

    [DONE] Auth API doc 완료

---
+++ Server/app/docs/auth.py
@@ -0,0 +1,78 @@
+CHECK_EMAIL_IS_CERTIFIED_GET={
+    'tags':['[Auth] 이메일 인증 체크'],
+    'description':'해당 이메일이 인증된 이메일인지 체크합니다',
+    'parameters':'email',
+    'response':{
+        '200':{
+            'description':'인증된 이메일 입니다'
+        },
+        '204':{
+            'description':'인증되지 않은 이메일 입니다'
+        }
+    }
+}
+AUTH_POST={
+    'tags':['[Auth] 로그인'],
+    'description':'유저 로그인',
+    'parameters':[
+        {
+            'name':'email',
+            'description':'유저 이메일',
+            'in':'json',
+            'type':'str',
+            'required':True
+        },
+        {
+            'name':'pw',
+            'description':'유저 비밀 번호',
+            'in':'json',
+            'type':'str',
+            'required':True
+        }
+    ],
+    'response':{
+        '200':{
+            'description':'로그인 성공',
+            'examples':{
+                'accessToken':'<accessToken>',
+                'refreshToken':'<refreshToken>'
+            }
+        },
+        '204':{
+            'description':'인증되지 않은 이메일'
+        },
+        '205':{
+            'description':'필수 정보가 입력되지 않음'
+        },
+        '401':{
+            'description':'로그인 실패'
+        }
+    }
+}
+REFRESH_GET={
+    'tags':['[Auth] 토근 재발급'],
+    'description':'토큰을 재발급합니다',
+    'parameters':[
+        {
+            'name':'Authorization',
+            'description':'Refresh Token',
+            'in':'header',
+            'type':'str',
+            'required':True
+        }
+    ],
+    'response':{
+        '200':{
+            'description':'토큰 재발급 성공',
+            'example':{
+                'accessToken':'<accessToken>'
+            }
+        },
+        '205':{
+            'description':'재로그인 필요'
+        },
+        '401':{
+            'description':'토큰 인증 실패'
+        }
+    }
+}
